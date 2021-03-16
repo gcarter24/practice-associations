8bfd6f5d566872e99c23328cc0874ea9520ed5bf
George <gchriscarter@gmail.com>
2021-03-16 15:01:54 -0400

    add validations to meeting and speaker

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,5 @@
 class Meeting < ApplicationRecord
+  validates :title, :agenda, :location, :time,  presence: true
   has_many :meeting_speakers
   has_many :speakers, through: :meeting_speakers
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,5 @@
 class Speaker < ApplicationRecord
   has_many :meeting_speakers
   has_many :meetings, through: :meeting_speakers
+  validates :first_name, :last_name, :email, presence: true
 end
